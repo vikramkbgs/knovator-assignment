27f16a433b54df206ef3b91d57bab583f6261f3e
vikramkbgs <vikramkbgs@gmail.com>
2024-04-20 11:48:12 +0530

    adding logic for login and sigup

---
+++ controllers/authController.js
@@ -0,0 +1,9 @@
+const User = require('../models/userModel');
+
+exports.register = async (req, res) => {
+    // Logic to register a new user
+};
+
+exports.login = async (req, res) => {
+    // Logic to authenticate user login
+};
