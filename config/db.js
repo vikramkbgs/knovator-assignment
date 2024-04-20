a32591b59b0ead7febe4f83e86fcfbf4837ded66
vikramkbgs <vikramkbgs@gmail.com>
2024-04-20 11:47:01 +0530

    setup mongodb

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/knovator', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+module.exports = mongoose.connection;
