470f31d68694dcf9fe61011905b1acb6f5a9ed27
vikramkbgs <vikramkbgs@gmail.com>
2024-04-20 16:54:39 +0530

    fixing middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,13 @@
 const passport = require('../config/passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel')
+
 
 const authenticateJWT = async (req, res, next) => {
     try {
+        // console.log(req.headers);
         // Get the token from the request headers
-        const token = req.headers.authorization.split(' ')[1];
+        const token = req.headers.token;
 
         // Verify the token
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
