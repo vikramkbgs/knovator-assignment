9a0bdcae56a947c8bd13a731285ac9206cce6f8b
vikramkbgs <vikramkbgs@gmail.com>
2024-04-20 12:25:41 +0530

    enviroment variable setup

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const db = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
 // Use routes
-app.use('/api/auth', authRoutes);
+app.use('/api', authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
